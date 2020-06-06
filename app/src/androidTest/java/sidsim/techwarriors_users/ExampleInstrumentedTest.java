c3be4323a0bc2fe5049666dbae4f1cc2feb85323
siddharth1597 <sidd15597@gmail.com>
2020-06-07 00:08:14 +0530

    Final package of Warrior19 for Users

--- app/src/androidTest/java/sidsim/techwarriors_users/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/sidsim/techwarriors_users/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package sidsim.techwarriors;
+package sidsim.techwarriors_users;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
--- app/src/test/java/sidsim/techwarriors_users/ExampleUnitTest.java
+++ app/src/test/java/sidsim/techwarriors_users/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package sidsim.techwarriors;
+package sidsim.techwarriors_users;
 
 import org.junit.Test;
 
